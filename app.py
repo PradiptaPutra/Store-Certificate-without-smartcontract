ef1faa68ddd8fce212f373dab4b8bc207d7e8d33
Pradipta Putra <20523004@students.uii.ac.id>
2023-05-11 11:15:33 +0700

    Final File

--- app.py
+++ app.py
@@ -10,8 +10,14 @@ app = Flask(__name__, static_folder='static')
 
 CORS(app)
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/index')
 def index():
-     return render_template("index.html")
+     return render_template('index.html') 
+
+
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
         self.index = index
